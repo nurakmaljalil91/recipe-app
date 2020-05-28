41b6b2e63da6f408cbaa5bd1117c431af39dfff4
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2020-05-28 22:52:13 +0800

    add recipe detail

--- app/src/main/java/com/nurakmaljalil91/recipeapp/RecipeAdapter.kt
+++ app/src/main/java/com/nurakmaljalil91/recipeapp/RecipeAdapter.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.squareup.picasso.Picasso
 
 class RecipeAdapter(private val context: Context,
@@ -15,6 +17,16 @@ class RecipeAdapter(private val context: Context,
     private val inflater: LayoutInflater
             = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
+    companion object {
+        private val LABEL_COLORS = hashMapOf(
+            "Low-Carb" to R.color.colorLowCarb,
+            "Low-Fat" to R.color.colorLowFat,
+            "Low-Sodium" to R.color.colorLowSodium,
+            "Medium-Carb" to R.color.colorMediumCarb,
+            "Vegetarian" to R.color.colorVegetarian,
+            "Balanced" to R.color.colorBalanced
+        )
+    }
 
 
     override fun getItem(position: Int): Any {
@@ -49,6 +61,18 @@ class RecipeAdapter(private val context: Context,
 
         Picasso.with(context).load(recipe.imageUrl).placeholder(R.mipmap.ic_launcher).into(thumbnailImageView)
         Picasso.with(context).setLoggingEnabled(true)
+
+
+        val titleTypeFace = ResourcesCompat.getFont(context, R.font.josefinsans_bold)
+        titleTextView.typeface = titleTypeFace
+
+        val subtitleTypeFace = ResourcesCompat.getFont(context, R.font.josefinsans_semibolditalic)
+        subtitleTextView.typeface = subtitleTypeFace
+
+        val detailTypeFace = ResourcesCompat.getFont(context, R.font.quicksand_bold)
+        detailTextView.typeface = detailTypeFace
+        detailTextView.setTextColor(ContextCompat.getColor(context, LABEL_COLORS[recipe.label] ?: R.color.colorPrimary))
+
 
 
         return rowView
---
+++ app/src/main/java/com/nurakmaljalil91/recipeapp/RecipeDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.nurakmaljalil91.recipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_detail)
+    }
+}
